c7ee74c206bed63bbec61a080678fbd9d1f5c01a
asifAjKhan <akhan006584@gmail.com>
2026-02-25 12:12:13 +0600

    hambarger menu complete with farmer motion. open and close animation added

---
+++ src/app/testanimation/page.jsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
+const Test = () => {
+  const variants = {
+    variant1: {
+      x: 400,
+      y: 300,
+      opacity: 0.5,
+      transition: { delay: 2, duration: 4 },
+    },
+
+    variant2: {
+      x: 100,
+      y: -300,
+      rotation: 90,
+    },
+  }
+
+  return (
+    <div className='h-full overflow-hidden flex items-center justify-center'>
+      <motion.div
+        className='w-96 h-96 bg-red-400 rounded'
+        initial={{ x: -100 }}
+        variants={variants}
+        animate={'variant1'}
+        transition={{ delay: 2, duration: 4 }}
+      ></motion.div>
+    </div>
+  )
+}
+
+export default Test
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,6 +5,8 @@ import { useState } from 'react'
 import Image from 'next/image'
 import NavLink from './navLink'
 
+import { motion } from 'framer-motion'
+
 const Links = [
   {
     title: 'Home',
@@ -26,6 +28,45 @@ const Links = [
 
 const Navbar = () => {
   const [open, setOpen] = useState(false)
+
+  const topVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: 45,
+      backgroundColor: 'rgb(255,255,255)',
+    },
+  }
+
+  const centerVariants = {
+    closed: {
+      opacity: 1,
+    },
+    opened: {
+      opacity: 0,
+    },
+  }
+
+  const bottomVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: -45,
+      backgroundColor: 'rgb(255,255,255)',
+    },
+  }
+
+  const listVariants = {
+    closed: {
+      x: '100vw',
+    },
+    opened: {
+      x: 0,
+    },
+  }
+
   return (
     <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl'>
       {/* links */}
@@ -77,22 +118,39 @@ const Navbar = () => {
           className='w-10 h-8 flex flex-col justify-between z-50 relative'
           onClick={() => setOpen((prev) => !prev)}
         >
-          <div className='w-10 h-1 bg-white rounded '></div>
+          <motion.div
+            variants={topVariants}
+            animate={open ? 'opened' : 'closed'}
+            className='w-10 h-1 bg-black rounded origin-left '
+          ></motion.div>
 
-          <div className='w-10 h-1 bg-white rounded '></div>
+          <motion.div
+            variants={centerVariants}
+            animate={open ? 'opened' : 'closed'}
+            className='w-10 h-1 bg-black rounded  '
+          ></motion.div>
 
-          <div className='w-10 h-1 bg-white rounded '></div>
+          <motion.div
+            variants={bottomVariants}
+            animate={open ? 'opened' : 'closed'}
+            className='w-10 h-1 bg-black rounded origin-left '
+          ></motion.div>
         </button>
 
         {/*MENU LIST */}
         {open && (
-          <div className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'>
+          <motion.div
+            variants={listVariants}
+            initial='closed'
+            animate='opened'
+            className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40'
+          >
             {Links.map((link) => (
               <Link href={link.url} key={link.title}>
                 {link.title}
               </Link>
             ))}
-          </div>
+          </motion.div>
         )}
       </div>
     </div>
