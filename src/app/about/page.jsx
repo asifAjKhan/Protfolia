4be0a54d89fe8b51bf626e82d51906304e3c7594
asifAjKhan <akhan006584@gmail.com>
2026-02-27 17:33:40 +0600

    page transition complete with motion

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,19 @@
+'use client'
+
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <motion.div
+      className='h-full '
+      initial={{ y: '-200vh' }}
+      animate={{ y: '0%' }}
+      transition={{ duration: 1 }}
+    >
+      <div>about</div>
+    </motion.div>
+  )
 }
 
 export default About
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,18 @@
+'use client'
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const Contract = () => {
-  return <div>Contract</div>
+  return (
+    <motion.div
+      className='h-full '
+      initial={{ y: '-200vh' }}
+      animate={{ y: '0%' }}
+      transition={{ duration: 1 }}
+    >
+      <div>contract</div>
+    </motion.div>
+  )
 }
 
 export default Contract
